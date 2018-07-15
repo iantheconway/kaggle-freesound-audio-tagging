1c05b4327b6cca490aba450f2af0730f494607c8
Ian Conway <iantheconway@gmail.com>
2018-07-14 17:16:55 -0700

    testing with default generator from zafar notebook

--- freesound.py
+++ freesound.py
@@ -18,9 +18,31 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from keras import losses, models, optimizers
 from keras.activations import relu, softmax
+from keras.utils import Sequence, to_categorical
 import tensorflow as tf
 
 np.random.seed(1001)
+
+class Config(object):
+    def __init__(self,
+                 sampling_rate=16000, audio_duration=2, n_classes=41,
+                 use_mfcc=False, n_folds=10, learning_rate=0.0001,
+                 max_epochs=50, n_mfcc=20):
+        self.sampling_rate = sampling_rate
+        self.audio_duration = audio_duration
+        self.n_classes = n_classes
+        self.use_mfcc = use_mfcc
+        self.n_mfcc = n_mfcc
+        self.n_folds = n_folds
+        self.learning_rate = learning_rate
+        self.max_epochs = max_epochs
+
+        self.audio_length = self.sampling_rate * self.audio_duration
+        if self.use_mfcc:
+            self.dim = (self.n_mfcc, 1 + int(np.floor(self.audio_length/512)), 1)
+        else:
+            self.dim = (self.audio_length, 1)
+
 
 # Mean average percision code from:
 # https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
@@ -324,6 +346,89 @@ class SoundClassifier(object):
             if accuracy > self.best_accuracy:
                 self.best_accuracy = accuracy
 
+    def prepare_data(self, df, config, data_dir):
+        X = np.empty(shape=(df.shape[0], config.dim[0], config.dim[1], 1))
+        input_length = config.audio_length
+        for i, fname in enumerate(df.index):
+            print(fname)
+            file_path = data_dir + fname
+            data, _ = librosa.core.load(file_path, sr=config.sampling_rate, res_type="kaiser_fast")
+
+            # Random offset / Padding
+            if len(data) > input_length:
+                max_offset = len(data) - input_length
+                offset = np.random.randint(max_offset)
+                data = data[offset:(input_length + offset)]
+            else:
+                if input_length > len(data):
+                    max_offset = input_length - len(data)
+                    offset = np.random.randint(max_offset)
+                else:
+                    offset = 0
+                data = np.pad(data, (offset, input_length - len(data) - offset), "constant")
+
+            data = librosa.feature.mfcc(data, sr=config.sampling_rate, n_mfcc=config.n_mfcc)
+            data = np.expand_dims(data, axis=-1)
+            X[i,] = data
+        return X
+
+    def train(self):
+        train = pd.read_csv("./train.csv")
+        test = pd.read_csv("./sample_submission.csv")
+        LABELS = list(train.label.unique())
+        label_idx = {label: i for i, label in enumerate(LABELS)}
+        train.set_index("fname", inplace=True)
+        test.set_index("fname", inplace=True)
+        train["label_idx"] = train.label.apply(lambda x: label_idx[x])
+        config = Config(sampling_rate=44100, audio_duration=2, n_folds=10,
+                        learning_rate=0.001, use_mfcc=True, n_mfcc=40)
+        X_train = self.prepare_data(train, config, '../input/freesound-audio-tagging/audio_train/')
+        X_test = self.prepare_data(test, config, '../input/freesound-audio-tagging/audio_test/')
+        y_train = to_categorical(train.label_idx, num_classes=config.n_classes)
+
+        mean = np.mean(X_train, axis=0)
+        std = np.std(X_train, axis=0)
+
+        X_train = (X_train - mean) / std
+        X_test = (X_test - mean) / std
+
+        PREDICTION_FOLDER = "predictions_2d_conv"
+        if not os.path.exists(PREDICTION_FOLDER):
+            os.mkdir(PREDICTION_FOLDER)
+        if os.path.exists('logs/' + PREDICTION_FOLDER):
+            shutil.rmtree('logs/' + PREDICTION_FOLDER)
+
+        skf = StratifiedKFold(train.label_idx, n_folds=config.n_folds)
+        for i, (train_split, val_split) in enumerate(skf):
+            keras.clear_session()
+            X, y, X_val, y_val = X_train[train_split], y_train[train_split], X_train[val_split], y_train[val_split]
+            checkpoint = keras.callbacks.ModelCheckpoint('best_%d.h5' % i, monitor='val_loss', verbose=1, save_best_only=True)
+            early = keras.callbacks.EarlyStopping(monitor="val_loss", mode="min", patience=5)
+            tb = keras.callbacks.TensorBoard(log_dir='./logs/' + PREDICTION_FOLDER + '/fold_%i' % i, write_graph=True)
+            callbacks_list = [checkpoint, early, tb]
+            print("#" * 50)
+            print("Fold: ", i)
+            model = self.get_2d_conv_model(config)
+            history = model.fit(X, y, validation_data=(X_val, y_val), callbacks=callbacks_list,
+                                batch_size=64, epochs=config.max_epochs)
+            model.load_weights('best_%d.h5' % i)
+
+            # Save train predictions
+            predictions = model.predict(X_train, batch_size=64, verbose=1)
+            np.save(PREDICTION_FOLDER + "/train_predictions_%d.npy" % i, predictions)
+
+            # Save test predictions
+            predictions = model.predict(X_test, batch_size=64, verbose=1)
+            np.save(PREDICTION_FOLDER + "/test_predictions_%d.npy" % i, predictions)
+
+            # Make a submission file
+            top_3 = np.array(LABELS)[np.argsort(-predictions, axis=1)[:, :3]]
+            predicted_labels = [' '.join(list(x)) for x in top_3]
+            test['label'] = predicted_labels
+            test[['label']].to_csv(PREDICTION_FOLDER + "/predictions_%d.csv" % i)
+
+
+
 
 def gpyopt_helper(x):
     """Objective function for GPyOpt.
@@ -334,7 +439,8 @@ def gpyopt_helper(x):
 
     sc = SoundClassifier()
     sc.set_params(x[0])
-    sc.train_tf_records()
+    # sc.train_tf_records()
+    sc.train()
     # Convert accuracy to error
     error = 1 - sc.best_accuracy
     return np.array([[error]])
